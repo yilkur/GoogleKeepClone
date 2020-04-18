5e50e7912bb7a46b3a39b4cfd6428fd3166b3c9c
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-04-18 13:12:08 +0200

    add note storage functionality

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 class App {
     constructor() {
+        this.notes = []
+
         this.$form = document.querySelector('#form')
         this.$noteTitle = document.querySelector('#note-title')
+        this.$noteText = document.querySelector('#note-text')
         this.$formButtons = document.querySelector('#form-buttons')
         this.addEventListeners()
     }
@@ -9,6 +12,17 @@ class App {
     addEventListeners() {
         document.body.addEventListener('click', event => {
             this.handleFormClick(event)
+        })
+
+        this.$form.addEventListener('submit', event => {
+            event.preventDefault()
+            const title = this.$noteTitle
+            const text = this.$noteText
+            const hasNote = title || text
+
+            if (hasNote) {
+                this.addNote({ title, text })
+            }
         })
     }
 
@@ -29,6 +43,16 @@ class App {
         this.$form.classList.remove('form-open')
         this.$noteTitle.style.display = 'none'
         this.$formButtons.style.display = 'none'
+    }
+
+    addNote(note) {
+        const newNote = {
+            title: note.title,
+            text: note.text,
+            color: 'white',
+            id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+        }
+        this.notes = [...this.notes, newNote]
     }
 }
 
