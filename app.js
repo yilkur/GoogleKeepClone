1fdcb1588652f39898c886f8dd6fcfbce80cabd6
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-04-19 12:48:15 +0200

    add display notes functionality

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ class App {
     constructor() {
         this.notes = []
 
+        this.$notes = document.querySelector('#notes')
+        this.$placeholder = document.querySelector('#placeholder')
         this.$form = document.querySelector('#form')
         this.$noteTitle = document.querySelector('#note-title')
         this.$noteText = document.querySelector('#note-text')
         this.$formButtons = document.querySelector('#form-buttons')
+
         this.addEventListeners()
     }
 
@@ -16,8 +19,8 @@ class App {
 
         this.$form.addEventListener('submit', event => {
             event.preventDefault()
-            const title = this.$noteTitle
-            const text = this.$noteText
+            const title = this.$noteTitle.value
+            const text = this.$noteText.value
             const hasNote = title || text
 
             if (hasNote) {
@@ -43,6 +46,8 @@ class App {
         this.$form.classList.remove('form-open')
         this.$noteTitle.style.display = 'none'
         this.$formButtons.style.display = 'none'
+        this.$noteTitle.value = ''
+        this.$noteText.value = ''
     }
 
     addNote(note) {
@@ -53,6 +58,26 @@ class App {
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
         }
         this.notes = [...this.notes, newNote]
+        this.displayNotes()
+        this.closeForm()
+    }
+
+    displayNotes() {
+        const hasNotes = this.notes.length > 0
+        this.$placeholder.style.display = hasNotes ? 'none' : 'flex'
+
+        this.$notes.innerHTML = this.notes.map(note => `
+            <div style="background: ${note.color};" class="note">
+                <div class="${note.title && 'note-title'}">${note.title}</div>
+                <div class="note-text">${note.text}</div>
+                <div class="toolbar-container">
+                    <div class="toolbar">
+                        <img class="toolbar-color" src="https://icon.now.sh/palette">
+                        <img class="toolbar-delete" src="https://icon.now.sh/delete">
+                    </div>
+                </div>
+            </div>
+        `).join('')
     }
 }
 
