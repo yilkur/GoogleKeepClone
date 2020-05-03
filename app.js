e37d6b2269b7bdaff75214591c2d30f5084af1c8
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-05-03 15:54:14 +0200

    add delete feature

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ class App {
             this.handleFormClick(event)
             this.selectNote(event)
             this.openModal(event)
+            this.deleteNote(event)
         })
 
         document.body.addEventListener('mouseover', event => {
@@ -101,6 +102,8 @@ class App {
     }
 
     openModal(event) {
+        if (event.target.matches('.toolbar-delete')) return
+
         if (event.target.closest('.note')) {
             this.$modal.classList.toggle('open-modal')
             this.$modalTitle.value = this.title
@@ -166,6 +169,14 @@ class App {
         this.id = $selectedNote.dataset.id
     }
 
+    deleteNote(event) {
+        event.stopPropagation()
+        if (!event.target.matches('.toolbar-delete')) return
+        const id = event.target.dataset.id
+        this.notes = this.notes.filter(note => note.id !== +id)
+        this.displayNotes()
+    }
+
     displayNotes() {
         const hasNotes = this.notes.length > 0
         this.$placeholder.style.display = hasNotes ? 'none' : 'flex'
@@ -179,7 +190,7 @@ class App {
                     <div class="toolbar-container">
                         <div class="toolbar">
                             <img class="toolbar-color" data-id=${note.id} src="https://icon.now.sh/palette">
-                            <img class="toolbar-delete" src="https://icon.now.sh/delete">
+                            <img class="toolbar-delete" data-id=${note.id} src="https://icon.now.sh/delete">
                         </div>
                     </div>
                 </div>
